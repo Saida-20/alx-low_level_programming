fa272a60d8c65785421a4e044fc8c119cdcc0c6a
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-08 14:29:15 -0700

    size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 /*
  * file: 6-size.c
  */
+
 #include <stdio.h>
 
 /**
  * main - prints the size of various types on the computer
  * it is compiled and run on.
  *
- * Return - 0 always.
+ * Return: Always 0.
  */
 int main(void)
 {
