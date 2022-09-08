d58d839224a96f7c6f2bfa4d020b3538567bbba8
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-08 12:42:54 -0700

    hello puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- * main - Entry point
+ * main - prints "Programming is like building a multilingual puzzle,followed by a new line.
  *
  *Return: Always 0 (Success)
  */
@@ -10,7 +9,7 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 }
 
 
