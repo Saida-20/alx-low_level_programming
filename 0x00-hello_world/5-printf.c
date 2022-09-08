589931ba06b2a61be4c4f9f8e2c2885c41bafad6
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-08 14:00:20 -0700

    hello printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * file: 5-printf.c
+ */
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art" followed by a new line.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
