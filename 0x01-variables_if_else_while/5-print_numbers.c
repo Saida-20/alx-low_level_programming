dc38ac4408f81827025f093742a3fc05edcb4492
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-09 12:03:53 -0700

    numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+ * file: 5-print_numbers.c
+ */
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	
+	printf("\n");
+	return (0);
+}
