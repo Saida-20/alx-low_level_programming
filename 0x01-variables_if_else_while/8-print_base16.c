c3c80afedcb16818b8db40fdd637a0d50d742939
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-09 15:09:39 -0700

    hexadecimal task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/*
+ * file: 8-print_base16.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by
+ * a new line.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int num;
+	char alphabet;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
