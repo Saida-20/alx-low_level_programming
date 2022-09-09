faac50799915007cf3201fc0659831bded589e6a
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-09 12:57:51 -0700

    smile in the mirror task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*
+ * file: 7-print_tebahpla.c
+ */
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: 0always.
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = ''z'; alphabets >= 'a'; alphabet--)
+		putchar(alphabets);
+
+	putchar('\n');
+	return (0);
+}
