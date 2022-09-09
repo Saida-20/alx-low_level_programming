a2cfaafadacb795a4e9c7adc75f4876c8d320a1f
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-09 11:17:21 -0700

    alphabets task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * file: 2-print_alphabet.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case.
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets<= 'z'; alphabets++)
+		putchar(alphabets);
+
+	putchar('\n')
+
+	return(0);
+}
