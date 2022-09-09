ace5b3e2bbebfc742718dc0c8a74239fe176743b
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-09 10:39:44 -0700

    last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
 /**
  * main - prints the last digit of the number stored in the variable n.
  *
- * Return: 0 always.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -20,17 +20,17 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+				n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
+				n, n % 10);
 	}
 	else
 	{
 		printf("Last digit of %d is %d and is 0\n ",
-			n, n % 10);
+				n, n % 10);
 	}
 	return (0);
 }		
