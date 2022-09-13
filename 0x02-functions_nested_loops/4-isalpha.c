287ff06b7be15ada8ebf6f162fd7ca264861bb86
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 16:25:55 -0700

    is alpha task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/*
+ * file: 4-isalpha.c
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is alphabetic.
+ * @C - the character to be checked
+ *
+ * Return: 0 always.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c<= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+	       return (0);
+}	
+
