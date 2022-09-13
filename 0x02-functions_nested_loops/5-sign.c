2b6119f713da4f0f006befac7bc6c81ad2683e37
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 16:43:31 -0700

    sign task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,10 +12,11 @@
  * 0 if number is zero,
  * -1 if number is less than zero
  */
- int print_sign(int n)
-{ if (n > 0)
+int print_sign(int n)
+{
+	if (n > 0)
 	{
-	       	_putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -26,6 +27,6 @@
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
