98ab14d8445cc9779a5089a61dd4e5be3d807b8c
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 16:44:47 -0700

    abs task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/*
+ * file: 6-abs.c
+ */
+
+#include "main.h"
+
+/** _abs - computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: absolute value ofan integer.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
