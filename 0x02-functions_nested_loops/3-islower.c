6529d0efbc7f3e37f14048d03cbb01c6dc7b3d61
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 16:14:37 -0700

    is lower task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/*
+ * file: 3-islower.c
+ */
+
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase.
+ * @c : the character to be checked.
+ *
+ * Return: 0 always.
+ */
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
