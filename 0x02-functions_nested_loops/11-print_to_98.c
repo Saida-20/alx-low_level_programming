c090f497f7c5916c98cdae2067677ed467326eec
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 17:11:58 -0700

    battery street task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+/*
+ * file: 11-print_to_98.c
+ */
+
+#include "main.h"
+
+/**
+ * print_to_98 -  prints all natural numbers from n to 98, Numbers must 
+ * be separated by a comma, followed by a space
+ * @n - number to begin couting at.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d", ", n--);
+		printf("%\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
+
