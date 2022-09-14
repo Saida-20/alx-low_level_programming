2e0c9f15bcd4069f51ba89e050fda06ceef79791
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-13 17:06:36 -0700

    a+b task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * file: 10-add.c
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1 - first integer number
+ * @num2 - second integer number
+ *
+ * Return result of addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
