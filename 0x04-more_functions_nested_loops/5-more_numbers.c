cf135f8d9c89518921d3a4878b496e75e4521349
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-15 11:17:07 -0700

    more numbers task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@
 void more_numbers(void)
 {
 	int c, i;
-	
+
 	c = 0;
 	while (c < 10)
 	{
@@ -17,7 +17,6 @@ void more_numbers(void)
 		{
 			if (i > 9)
 				_putchar(i / 10 + '0');
-			
 			_putchar(i % 10 + '0');
 			i++;
 		}
