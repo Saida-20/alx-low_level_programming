5f9bf18d3f3eee2e04b972f892613e1c52db71f5
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-15 07:20:59 -0700

    is digit task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type parameter
+ *
+ * Return: 1 if digit, else 0
+ */
+
+int _isdigit(int c);
+{
+	if(c > 47 && c < 55)
+		return (0);
+
+	return (1);
+}
