a39b2ec96fc8ad59f5c40bde2da401fd6073e601
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-15 09:21:20 -0700

    is upper task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - function that checks for uppercase characters
  * @c: int type number
- * Return: 1 if uppercase else, 0.
+ * Return: 1 if uppercase, else 0.
  */
-
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)
 		return (0);
+	
 	return (1);
 }
