7570c15a42d52c5a1f69a43d858a9a6fdd484e18
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-09-19 11:36:14 -0700

    atoi task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int j, k;
+
+	j = 0;
+	k = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			k *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (j < 0)
+				j = (j * 10) - (s[i] - '0');
+			else
+				j = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (k < 0)
+		j *= -1;
+	
+	return (j);
+}
