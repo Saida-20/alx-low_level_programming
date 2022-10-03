fe2b444d62ff96083ec986d0da2740d8bb48899d
Saida-20 <111362877+Saida-20@users.no reply.github.com>
2022-10-03 09:58:25 -0700

    string duplicate task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,20 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, size = 0;
-	char *m;
+	char *strout;
+	unsigned int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	for (; str[size] != '\0'; size++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
-	/*+1 on the size puts the end of string character*/
-	m = malloc(size * sizeof(*str) + 1);
 
-	if (m == 0)
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
 		return (NULL);
-	else
-	{
-		for (; i < size; i++)
-			m[i] = str[i];
-	}
-	return (m);
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
 }
